3e9cc1501b76040ae7db3b34828ebc54598f684e
Constant Dupuis <31339604+constantdupuis@users.noreply.github.com>
2020-10-30 20:49:55 +0100

    Got NgRx sample working, with Angular Material

--- un/src/app/app.module.ts
+++ un/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
+import { MatButtonModule } from '@angular/material/button'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCounterComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    StoreModule.forRoot({ count: counterReducer }),
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
